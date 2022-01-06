66e64b828fabb0f4824f4b49b0b638f0a64a0626
XiaYu <xiayu32110@163.com>
2022-01-06 10:19:04 +0800

    6th week

--- 最后一个单词的长度/lengthOfLastWord.py
+++ 最后一个单词的长度/lengthOfLastWord.py
@@ -6,6 +6,16 @@ class Solution:
         slist = s.split(' ')
         return len(slist[-1])
 
+    def lengthOfLastWord1(self, s: str) -> int:
+        l = 0
+        i = len(s) - 1
+        while(s[i] == ' '):
+            i -= 1
+        while(s[i] != ' ' and i >= 0):
+            l += 1
+            i -= 1
+        return l
+
 s = Solution()
-res = s.lengthOfLastWord("luffy is still joyboy")
+res = s.lengthOfLastWord1("   fly me   to   the moon  ")
 print(res)
