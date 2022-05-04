107ee4af88769b6cfccfa470428cf74636432138
XiaYu <xiayu32110@163.com>
2022-05-04 22:22:20 +0800

    21th week

---
+++ 3的幂/isPowerOfThree.py
@@ -0,0 +1,14 @@
+import math
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while(n > 1):
+            if n % 3 != 0:
+                return False
+            n = n / 3
+        return True
+
+s = Solution()
+print(s.isPowerOfThree(12))
