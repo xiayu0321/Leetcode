9801e11ee0da4226baabbc6a02e566a00918b3fc
XiaYu <xiayu32110@163.com>
2022-05-23 10:09:18 +0800

    24th week

---
+++ Excel表列名称/convertToTitle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = ""
+        while(columnNumber > 0):
+               n = (columnNumber - 1 ) % 26
+               res += chr((ord('A') + n))
+               columnNumber = (columnNumber - n - 1) // 26
+        return res[::-1]
+
+s = Solution()
+print(s.convertToTitle(701))
